4fea71ed2cf50c1085eb0f1b08b0de1618d19f61
fanovivan <fanovivan@yandex.ru>
2026-05-05 16:53:04 +0300

    startproject

---
+++ breeders/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BreedersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'breeders'
---
+++ messages_ws/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MessagesWsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messages_ws'
