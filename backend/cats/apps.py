302a5707e58c8bb7dd153b37314520409b28c881
fanovivan <fanovivan@yandex.ru>
2026-05-08 15:06:53 +0300

    Add default cat breeds and ensure they are created on post-migrate
    - Introduced a list of default cat breeds to be created in the database.
    - Implemented a signal handler to ensure default breeds are added after migrations.
    - Updated CatsConfig to connect the signal handler during app readiness.

--- backend/cats/apps.py
+++ backend/cats/apps.py
@@ -1,6 +1,32 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+DEFAULT_BREEDS = (
+    "Сиамская",
+    "Мейн-кун",
+    "Британская",
+    "Дворовая",
+    "Сфинкс",
+    "Персидская",
+)
+
+
+def ensure_default_breeds(sender, **kwargs):
+    # Import inside signal handler to avoid touching models before app loading.
+    from .models import Breed
+
+    for name in DEFAULT_BREEDS:
+        Breed.objects.get_or_create(name=name)
 
 
 class CatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.cats'
+
+    def ready(self):
+        post_migrate.connect(
+            ensure_default_breeds,
+            sender=self,
+            dispatch_uid="cats.ensure_default_breeds",
+        )
